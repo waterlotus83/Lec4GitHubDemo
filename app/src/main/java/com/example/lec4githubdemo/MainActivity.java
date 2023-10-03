9bd5282a1437cd23758ec2c00fde9c1be7bf951b
Chow <chowh8@student.douglascollege.ca>
2023-10-03 09:54:41 -0700

    Initial commit

---
+++ app/src/main/java/com/example/lec4githubdemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.lec4githubdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Mode some changes locally
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
